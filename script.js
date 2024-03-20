88cc18d7b38ccdf16dbc8f451b1be838635d62cf
Jefferson Whitney <mendonca11@gmail.com>
2024-03-20 12:21:22 -0300

    adicionando web analytics

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const texto_input = document.querySelector("#texto__input").value;
-
-function temLetraMaiuscula(texto_input) {
-    console.log("funcionou");
-    return texto_input !== texto_input.toLowerCase();
+function temLetraMaiuscula() {
+    const texto_input = document.getElementById("texto__input").value;
+    console.log(texto_input);
+    // return texto_input !== texto_input.toLowerCase();
 
   }
   
-if (temLetraMaiuscula(texto_input)) {
-    console.log("O texto contém letras maiúsculas.");
-  } else {
-    console.log("O texto não contém letras maiúsculas.");
-  }
+// if (temLetraMaiuscula(texto_input)) {
+//     console.log("O texto contém letras maiúsculas.");
+//   } else {
+//     console.log("O texto não contém letras maiúsculas.");
+//   }
