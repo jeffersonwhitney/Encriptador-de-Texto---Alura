9886e6e79d907491358533da7688fc6b3c5bf5f5
Jefferson Whitney <mendonca11@gmail.com>
2024-03-20 13:04:40 -0300

    log mostrando maiuscula minuscula

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function temLetraMaiuscula() {
     const texto_input = document.getElementById("texto__input").value;
     console.log(texto_input);
-    // return texto_input !== texto_input.toLowerCase();
-
+    if (texto_input !== texto_input.toLowerCase()) {
+      console.log("O texto contém letras maiúsculas.");
+    } else {
+      console.log("O texto não contém letras maiúsculas.");
+    }
   }
   
-// if (temLetraMaiuscula(texto_input)) {
-//     console.log("O texto contém letras maiúsculas.");
-//   } else {
-//     console.log("O texto não contém letras maiúsculas.");
-//   }
